c53e0d1aaa11e7dac7b2485f970a45ef82afdab9
Marc Littlemore <marc.littlemore@gmail.com>
2012-11-13 15:12:34 +0000

    Testing code and some updates to API

--- AmazonAPI.php
+++ AmazonAPI.php
@@ -6,46 +6,23 @@
  * 
  *  Author: Marc Littlemore
  *  http://www.marclittlemore.com
- *  
- *  This code is free, provided AS-IS with no warranty expressed or implied.  Use at your own risk.
- *  If you find errors or bugs in this code, please contact me at interested@zanpo.com
- *  If you enhance this code in any way, please send me an update.  Thank you!
- *
- *  Version: 0.1
- *  Last Updated: 13/12/2009
  * 
- *	NOTE: ENTER YOUR API ID AND SECRET KEY BELOW!!!
  *
  */ 
 
 class AmazonAPI
 {
-
-	private $m_CI = NULL;		// The CodeIgniter instance
 	private $m_amazonUrl = '';
-	private $m_scraperUrl = '';
-	private $m_amazonBaseUrl = '';
 	private $m_locale = 'uk';
+	private $m_retrieveItems = false;
 
-	// The two locale tables should always support the same locales
-	// or we'll get into trouble! :)
+	// URL of each territory
 	private $m_localeTable = array(
 		'uk'	=>	'http://ecs.amazonaws.co.uk/onca/xml',
 		'de'	=>	'http://ecs.amazonaws.de/onca/xml',
 		'us'	=>	'http://ecs.amazonaws.com/onca/xml',
 	);
 
-	private $m_scraperLocaleTable = array(
-		'uk'	=>	'http://www.amazon.co.uk/exec/obidos/ASIN/',
-		'de'	=>	'http://www.amazon.de/exec/obidos/ASIN/',
-		'us'	=>	'http://www.amazon.com/exec/obidos/ASIN/',
-	);
-	
-	private $m_amazonBaseUrlLocaleTable = array(
-		'uk'	=>	'http://www.amazon.co.uk/',
-		'de'	=>	'http://www.amazon.de/',
-		'us'	=>	'http://www.amazon.com/',
-	);
 	// -----------------------------------------
 	// -----------------------------------------
 	// API key ID
@@ -63,18 +40,14 @@ class AmazonAPI
 
 	private $mErrors = array();
 
-	public function __construct( $keyId, $secretKey, $associateTag )
+	public function __construct( $keyId, $secretKey, $associateTag, $retrieveItems = false )
 	{
 		// Setup the AWS credentials
 		$this->m_keyId			= $keyId;
 		$this->m_secretKey		= $secretKey;
 		$this->m_associateTag	= $associateTag;
+		$this->m_retrieveItems	= $retrieveItems;
 
-		$this->m_CI =& get_instance();
-
-		// Pretend to be Google Chrome for scraping
-		ini_set( 'user_agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.64 Safari/532.5' );
-		
 		// Set UK as locale by default
 		$this->SetLocale( 'uk' );
 	}
@@ -102,8 +75,6 @@ class AmazonAPI
 		// Set the URL for this locale
 		$this->m_locale = $locale;
 		$this->m_amazonUrl = $this->m_localeTable[$locale];
-		$this->m_scraperUrl = $this->m_scraperLocaleTable[$locale];
-		$this->m_amazonBaseUrl = $this->m_amazonBaseUrlLocaleTable[$locale];
 	}
 	
 	public function GetValidSearchNames()
@@ -111,11 +82,6 @@ class AmazonAPI
 		return( $this->mValidSearchNames );
 	}
 
-	function GetScraperUrl()
-	{
-		return( $this->m_scraperUrl );
-	}
-	
 	private function GetUrl( $url )
 	{
 		// The use of `file_get_contents` may not work on all servers because it relies on the ability to open remote
@@ -127,7 +93,7 @@ class AmazonAPI
 		curl_setopt( $session, CURLOPT_RETURNTRANSFER, true );
 		$response = curl_exec( $session );
 		curl_close( $session ); 
-		//$response = file_get_contents($request);
+
 		$parsedXml = simplexml_load_string( $response );
 		
 		return( $parsedXml );
@@ -169,10 +135,15 @@ class AmazonAPI
 		// Get the response from the signed URL
 		$parsedXml = $this->GetUrl( $signedUrl );
 		
-		//$this->PrintErrors( $parsedXml );
-		//var_dump( $parsedXml );
+		if ( $this->m_retrieveItems )
+		{
+			$items = $this->RetrieveItems( $parsedXml );
+		}
+		else
+		{
+			$items = $parsedXml;
+		}
 
-		$items = $this->RetrieveItems( $parsedXml );
 		return( $items );
 	}
 	
@@ -207,11 +178,14 @@ class AmazonAPI
 		// Get the response from the signed URL
 		$parsedXml = $this->GetUrl( $signedUrl );
 		
-		//$this->PrintErrors( $parsedXml );
-//		var_dump( $parsedXml ); die();
-		
-		var_dump( $parsedXml->Items );
-		$items = $this->RetrieveItems( $parsedXml );
+		if ( $this->m_retrieveItems )
+		{
+			$items = $this->RetrieveItems( $parsedXml );
+		}
+		else
+		{
+			$items = $parsedXml;
+		}
 		return( $items );
 	}
 
@@ -262,8 +236,6 @@ class AmazonAPI
 			$item['mediumImage'] = (string) $responseItem->MediumImage->URL;
 			$item['smallImage'] = (string) $responseItem->SmallImage->URL;
 
-			// var_dump( $responseItem->EditorialReviews );
-			//var_dump( $item );
 			array_push( $items, $item );
 		}
 
@@ -361,5 +333,4 @@ class AmazonAPI
 		return( $errorString );
 	}
 }
-
 ?>
