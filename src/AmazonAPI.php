5303dbfc18260e570a5b6d00016009979e341cb0
Wasay Syed <wasay@witinc.com>
2018-03-10 23:42:28 -0500

    AmazonAPI.php: Moved protected variable array item to config file, and updated constructor to use passed arguments if the values are not empty.

--- src/AmazonAPI.php
+++ src/AmazonAPI.php
@@ -19,6 +19,7 @@ class AmazonAPI
 	private $dataTransformer = NULL;
 
 	// Valid names that can be used for search
+	// This variable can be set to NULL
 	private $mValidSearchNames = array(
 		'All',
 		'Apparel',
@@ -61,15 +62,35 @@ class AmazonAPI
 		'Watches'
 	);
 
+
+	private $mValidResponseGroups = NULL;
+	private $mValidSortValues = NULL;
+
 	private $mErrors = array();
 
-	public function __construct($urlBuilder, $outputType) {
+	public function __construct($urlBuilder, $outputType, $searchNames = NULL, $responseGroups = NULL, $sortValues = NULL) {
 		$this->urlBuilder = $urlBuilder;
 		$this->dataTransformer = DataTransformerFactory::create($outputType);
+
+		if ( ! empty($searchNames) )
+		{
+			$this->mValidSearchNames = $searchNames;
+		}
+
+		$this->mValidResponseGroups = $responseGroups;
+		$this->mValidSortValues = $sortValues;
 	}
 
 	public function GetValidSearchNames() {
 		return $this->mValidSearchNames;
+	}
+
+	public function GetValidResponseGroups() {
+		return $this->mValidResponseGroups;
+	}
+
+	public function GetValidSortValues() {
+		return $this->mValidSortValues;
 	}
 
 	/**
@@ -79,13 +100,21 @@ class AmazonAPI
 	 * @param	searchIndex			Name of search index (category) requested. NULL if searching all.
 	 * @param	sortBy				Category to sort by, only used if searchIndex is not 'All'
 	 * @param	condition			Condition of item. Valid conditions : Used, Collectible, Refurbished, All
+	 * @param	responseGroups			Response group's for results. Valid conditions : See mValidResponseGroups
 	 *
 	 * @return	mixed				SimpleXML object, array of data or false if failure.
 	 */
-	public function ItemSearch($keywords, $searchIndex = NULL, $sortBy = NULL, $condition = 'New') {
+	public function ItemSearch($keywords, $searchIndex = NULL, $sortBy = NULL, $condition = 'New', $responseGroups = NULL) {
+
+		if (empty($responseGroups))
+		{
+			// if empty, set default
+			$responseGroups = 'ItemAttributes,Offers,Images';
+		}
+
 		$params = array(
 			'Operation' => 'ItemSearch',
-			'ResponseGroup' => 'ItemAttributes,Offers,Images',
+			'ResponseGroup' => $responseGroups,
 			'Keywords' => $keywords,
 			'Condition' => $condition,
 			'SearchIndex' => empty($searchIndex) ? 'All' : $searchIndex,
@@ -100,18 +129,30 @@ class AmazonAPI
 	 *
 	 * @param	asinList			Either a single ASIN or an array of ASINs
 	 * @param	onlyFromAmazon		True if only requesting items from Amazon and not 3rd party vendors
+	 * @param	responseGroup		Response group for results. Valid conditions : See mValidResponseGroups
+	 * @param	reviewSort			Review sort order for results. Valid conditions : See mValidReviewSorts
 	 *
 	 * @return	mixed				SimpleXML object, array of data or false if failure.
 	 */
-	public function ItemLookup($asinList, $onlyFromAmazon = false) {
+	public function ItemLookup($asinList, $onlyFromAmazon = false, $responseGroup = NULL, $reviewSort = NULL) {
 		if (is_array($asinList)) {
 			$asinList = implode(',', $asinList);
+		}
+		if (empty($responseGroup))
+		{
+			// if empty, set default
+			$responseGroup = 'ItemAttributes,Offers,Reviews,Images,EditorialReview';
+		}
+		if (empty($reviewSort))
+		{
+			// if empty, set default
+			$reviewSort = '-OverallRating';
 		}
 
 		$params = array(
 			'Operation' => 'ItemLookup',
-			'ResponseGroup' => 'ItemAttributes,Offers,Reviews,Images,EditorialReview',
-			'ReviewSort' => '-OverallRating',
+			'ResponseGroup' => $responseGroup,
+			'ReviewSort' => $reviewSort,
 			'ItemId' => $asinList,
 			'MerchantId' => ($onlyFromAmazon == true) ? 'Amazon' : 'All'
 		);
